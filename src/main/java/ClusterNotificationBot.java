49d1784e646b8ba7b0c9c51072aec72a40711bae
Antonio <antoniorodrigo92@gmail.com>
2023-12-04 13:35:33 +0000

    initial commit

--- src/main/java/ClusterNotificationBot.java
+++ src/main/java/ClusterNotificationBot.java
@@ -1,18 +1,47 @@
+import org.eclipse.paho.client.mqttv3.MqttException;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
-import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+import org.yaml.snakeyaml.Yaml;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.Map;
+import java.util.concurrent.CyclicBarrier;
+
+
 
 public class ClusterNotificationBot {
 
-    public static void main(String[] args) {
-        String propertiesPath = args[0];
+    public static void main(String[] args) throws MqttException, FileNotFoundException {
+//        String propertiesPath = args[0];
+        String propertiesPath = "C:\\Users\\Antonio\\IdeaProjects\\ClusterNotificationBot\\src\\main\\resources\\properties.yaml";
+        InputStream inputStream = new FileInputStream(new File(propertiesPath));
+        Yaml yaml = new Yaml();
+        Map<String, Object> data = yaml.load(inputStream);
+
+        MosquittoInstance mosquittoInstance = null;
+        CyclicBarrier barrier = new CyclicBarrier(2);
+        System.out.println("BARREIRA CRIADA");
         try {
+            mosquittoInstance = new MosquittoInstance(
+                data.get("mosquitto.broker").toString(),
+                data.get("mosquitto.username").toString(),
+                data.get("mosquitto.password").toString(),
+                data.get("mosquitto.topic_resp").toString(),
+        "ClusterNotificationBot",
+                barrier
+            );
+            System.out.println("INSTANCIA MOSQUITTO CRIADA");
+            mosquittoInstance.startWaitForMessage();
+            System.out.println("JÁ ESTÁ A ESCUTAR");
+
             TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
-            botsApi.registerBot(new TelegramBotNotifiers(propertiesPath));
-        } catch (TelegramApiException | FileNotFoundException e) {
+            botsApi.registerBot(new TelegramBotNotifiers(propertiesPath, mosquittoInstance, barrier));
+        } catch (Exception e) {
             e.printStackTrace();
+            mosquittoInstance.closeClient();
         }
     }
 
---
+++ src/main/java/MosquittoInstance.java
@@ -0,0 +1,51 @@
+import org.eclipse.paho.client.mqttv3.*;
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
+
+import java.util.concurrent.*;
+
+public class MosquittoInstance {
+    private MqttClient client;
+    private String topic;
+    private CyclicBarrier barrier;
+    private String response;
+
+    public MosquittoInstance(String broker, String username, String password, String topic, String clientid, CyclicBarrier barrier) throws MqttException {
+        client = new MqttClient(broker, clientid, new MemoryPersistence());
+        this.topic = topic;
+        this.barrier = barrier;
+        MqttConnectOptions options = new MqttConnectOptions();
+        options.setUserName(username);
+        options.setPassword(password.toCharArray());
+        options.setConnectionTimeout(60);
+        options.setKeepAliveInterval(60);
+        client.connect(options);
+    }
+
+    public void sendMessage(String topic, String content, int qos) throws MqttException {
+        MqttMessage message = new MqttMessage(content.getBytes());
+        message.setQos(qos);
+        client.publish(topic, message);
+
+        System.out.println("sent message to " + topic + ": " + message);
+    }
+
+
+    public void startWaitForMessage() throws MqttException, InterruptedException, BrokenBarrierException, TimeoutException {
+        client.subscribe(topic, (topic, msg) -> {
+            this.response = msg.toString();
+            barrier.await();
+        });
+    }
+
+
+    public String getResponse() {
+        return this.response;
+    }
+
+
+    public void closeClient() throws MqttException {
+        client.disconnect();
+        client.close();
+    }
+
+}
